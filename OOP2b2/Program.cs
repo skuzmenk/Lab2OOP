1f86f9a7689d14f35281c415ffc5ba0f49bfcd9a
skuzmenk <kuzmenko.oleksandr1123@vu.cdu.edu.ua>
2024-10-27 19:11:08 +0200

    create numerator, denominator and add checking, normalize minus and abbreviation

--- OOP2b2/Program.cs
+++ OOP2b2/Program.cs
@@ -10,7 +10,35 @@ namespace OOP2b2
     {
         public class MyFrac
         {
+            public int Numerator { get; private set; }
+            public int Denominator { get; private set; }
+            public MyFrac(int numerator, int denominator)
+            {
+                if (denominator == 0)
+                {
+                    throw new ArgumentException("Denominator cannot be zero.");
+                }
 
+                if (denominator < 0)
+                {
+                    numerator = -numerator;
+                    denominator = -denominator;
+                }
+
+                int gcd = GCD(Math.Abs(numerator), Math.Abs(denominator));
+                Numerator = numerator / gcd;
+                Denominator = denominator / gcd;
+            }
+            private int GCD(int a, int b)
+            {
+                while (b != 0)
+                {
+                    int t = b;
+                    b = a % b;
+                    a = t;
+                }
+                return a;
+            }
         }
         static void Main(string[] args)
         {
